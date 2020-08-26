fee837a7779931db3f12c1ff677662eecfb4d480
Jeremy Rufo <jeremyhrufo@gmail.com>
2020-08-26 18:22:33 -0400

    Start up of the App; Created secret Keys class with bogus info

---
+++ TweetSense/SecretKeys.swift
@@ -0,0 +1,15 @@
+//
+//  SecretKeys.swift
+//  TweetSense
+//
+//  Created by Jeremy Rufo on 8/26/20.
+//  Copyright © 2020 JRufo. All rights reserved.
+//
+
+import Foundation
+
+struct SecretKeys {
+    
+    static let TWITTER_CONSUMER_KEY = "XXXXX"
+    static let TWITTER_CONSUMER_SECRET = "XXXXX"
+}
--- TweetSense/ViewController.swift
+++ TweetSense/ViewController.swift
@@ -1,28 +1,32 @@
 //
 //  ViewController.swift
-//  Twittermenti
+//  TweetSense
 //
-//  Created by Angela Yu on 17/07/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Jeremy Rufo on 08/25/2020
+//  Copyright © 2020 JRufo. All rights reserved.
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
 
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    let swifter = Swifter(consumerKey: SecretKeys.TWITTER_CONSUMER_KEY,
+                          consumerSecret: SecretKeys.TWITTER_CONSUMER_SECRET)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
-    }
-    
-}
 
+        
+    }
+}
